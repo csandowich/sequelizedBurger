ecf1e4f55063703c3c7e4d4af80e8887238488bc
Caleb Sandowich <calebsandowich@calebs-mbp.attlocal.net>
2017-08-20 20:05:02 -0700

    still not finished

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,10 @@
 
 module.exports = function(sequelize, DataTypes) {
-  var Burger = sequelize.define('Burger', {
+  var burger = sequelize.define('burger', {
     burger_name: DataTypes.STRING,
     devoured: {
     	type: DataTypes.BOOLEAN,
     	defaultValue: false
-    }
-  }, {
-    classMethods: {
-      associate: function(models) {
-        Burger.hasOne(models.Customer);
-      }
     }
   });
   return Burger;
